7ed8c4ac5d9c8af430165021cc588a7248c3c769
Juan Suarez Lacuesta <juansuarezla1998@gmail.com>
2022-10-21 14:01:03 -0300

    changes done

--- javascript/script.js
+++ javascript/script.js
@@ -1,32 +1,7 @@
-
-class MoviePremiers{
-    constructor(title, date, place, seats){
-        this.title = title;
-        this.date = date;
-        this.place = place;
-        this.seats = seats;
-    }
-    purchaseTicket(){
-        let amount = parseInt(prompt("How many seats would you like to purchase?")); 
-        let cont = this.seats - amount;
-        this.seats = cont;
-        const total = amount * 40;
-        alert("You purchased " + amount + "seats for " + this.title + "," + " your total for this purchase is: " + total + "$");
-    }
-}
-
-const movie1 = new MoviePremiers("Black Adam", "15/10", "Los Angeles", 23);
-const movie2 = new MoviePremiers("Avatar: Way of Water", "10/12", "Madrid", 209);
-const movie3 = new MoviePremiers("Blonde", "10/9", "Paris", 51);
-const movie4 = new MoviePremiers("Woman King", "12/9", "San Francisco", 0);
-const movie5 = new MoviePremiers("Black Panther", "5/11", "Los Angeles", 0);
-
-const selectionMovies = [movie1, movie2, movie3, movie4, movie5];
-
 function purchase (movie) {
-    for (let i = 0; i < 5; i++) { 
+    for (let i = 0; i < selectionMovies.length; i++) { 
         if (movie == selectionMovies[i].title) {
-            alert(selectionMovies[i].title + "has " + selectionMovies[i].seats + " seats aviable for the " + selectionMovies[i].date);
+            alert(selectionMovies[i].title + " has " + selectionMovies[i].seats +  " seats aviable for the " + selectionMovies[i].date);
             if (selectionMovies[i].seats <= 0) {
                 alert("TICKETS ARE SOLD OUT FOR THIS PREMIER");
             } else {
@@ -43,8 +18,45 @@ function purchase (movie) {
 
 
 
+class MoviePremiers{
+    constructor(title, date, place, seats){
+        this.title = title;
+        this.date = date;
+        this.place = place;
+        this.seats = seats;
+    }
+    purchaseTicket(){
+        let amount = parseInt(prompt("How many seats would you like to purchase?")); 
+        let cont = this.seats - amount;
+        this.seats = cont;
+        const total = amount * 40;
+        alert("You purchased " + amount + " seats for " + this.title + "," + " your total for this purchase is: " + total + "$");
+    }
+}
 
-const premier1 = prompt("Which premier you do you wish to attend?. These are the options: Black Adam, Avatar, Blonde, Woman King, Black Panther");
+const movie1 = new MoviePremiers("Black Adam", "15/10", "Los Angeles", 23);
+const movie2 = new MoviePremiers("Avatar: Way of Water", "10/12", "Madrid", 209);
+const movie3 = new MoviePremiers("Blonde", "10/9", "Paris", 51);
+const movie4 = new MoviePremiers("Woman King", "12/9", "San Francisco", 0);
+const movie5 = new MoviePremiers("Black Panther", "5/11", "Los Angeles", 0);
+
+const selectionMovies = [movie1, movie2, movie3, movie4, movie5];
+
+
+
+
+
+alert("Which premier you do you wish to attend?");
+
+let trueTitle = "";
+selectionMovies.forEach(element => {
+    let title = element.title;
+    trueTitle = trueTitle + title + ", ";
+});
+
+
+const premier1 = prompt (" These are the options: " + trueTitle);
+
 if (premier1 !== "salir") {
     purchase(premier1);
 }
