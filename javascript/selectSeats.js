c691e0e396b46ade456f37e232fd561936d2d502
Juan Suarez Lacuesta <juansuarezla1998@gmail.com>
2022-11-07 13:36:43 -0300

    fixed problems

--- javascript/selectSeats.js
+++ javascript/selectSeats.js
@@ -1,70 +1,48 @@
+
+
 let seats = document.getElementById("seats");
 const thisSeat = seats;
 
 const ticketStorage = localStorage.getItem("ticket");
 const ticket = JSON.parse(ticketStorage);
+const user = ticket[ticket.length - 1];
 
 let num1 = 0;
 let takenSeats = [];
 
 
-// function selectSeat(target) {
-//     if (target != 0) {
-//         takenSeats.push(target);
-//         console.log(takenSeats)
-//     }
-// }
 
 
-// function seletedSeats(id) {
-//     if (takenSeats.includes(id)) {
-//         takenSeats = takenSeats.filter((element) => {
-//             return element !== id;
-//         })
-//     } else {
-//         takenSeats.push(id);
-//     }
-//     console.log(takenSeats);
-// }
+console.log(user);
 
-
-
-
-ticket.forEach(element => {
-    num1 = num1 + 1;
-    if (num1 === ticket.length) {
-
-        let num2 = element.amountSeats;
-        console.log(num2);
-        if (takenSeats.length < num2) {
-            
-        }
-        
-    }
-
-});
 
 thisSeat.addEventListener("click", (e) => {
+    let seat = e.target; 
+    let targetId = parseInt(e.target.id);
+    console.log(targetId);
+    
+    if (!Number.isNaN(targetId)) {
         
-    let target = parseInt(e.target.id);
-    console.log(target);
-    
-    
-    
-    if (takenSeats.includes(target)) {
-        takenSeats = takenSeats.filter((element) => {
-            return element !== target;
-        })
-    } else {
+        if (takenSeats.includes(targetId)) {
 
-        takenSeats.push(target);
-    }
-    
-    
-    
-    
-    console.log(takenSeats);
+            seat.className = "bi bi-person-fill seats"
 
+            takenSeats = takenSeats.filter((element) => {
+                return element !== targetId;
+            
+            })
+        
+        } else {
+            
+            seat.className = "bi bi-person-fill seats seats-select"
+            takenSeats.push(targetId);
+        
+        }     
+        
+        console.log(takenSeats);
+    
+    } 
+    
 });
 
 
@@ -72,46 +50,6 @@ thisSeat.addEventListener("click", (e) => {
 
 
 
-
-// ticket.forEach(element => {
-//     num1 = num1 + 1;
-//     if (num1 === ticket.length) {
-
-//         let num2 = element.amountSeats;
-//         console.log(num2);
-//         if (takenSeats.length < num2) {
-            
-//         }
-        
-//     }
-
-// });
-
-// thisSeat.addEventListener("click", (e) => {
-        
-//     let targetID = parseInt(e.targetID.id);
-//     console.log(targetID);
-    
-    
-    
-//     if (takenSeats.includes(targetID)) {
-//         takenSeats = takenSeats.filter((element) => {
-//             return element !== targetID;
-//         })
-//     } else {
-
-//         if (takenSeats.length < num2) {
-//             // targetID.className = 
-//         }
-//         takenSeats.push(targetID);
-//     }
-    
-    
-    
-    
-//     console.log(takenSeats);
-
-// });
 
 
 
