95ae25b8a179e931b20e349139c2ae0c80483ad4
Juan Suarez Lacuesta <juansuarezla1998@gmail.com>
2022-11-17 17:10:57 -0300

    done

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-El archivo JS esta en la carpeta "javascript" por el nombre "script.js"
-Este solo es usado en la page "premier.html" no en el index.
+Buenas!!
+En este proyecto decidi hacer una reserva de lugares para premiers en el cine, todo esto esta en la pestania de "PREMIERS" no hay nada en el index!!
+La misma se puede acceder desde el menu hamburguesa de la derecha.
 
-Muchas gracias!!
+El tema reserva de asientos y todo esta en tres distintas html:
+
+1. En el primer html se coloca toda la informacion del cliente en cuestion, si falta algo de informacion saldra un alert indicando que se debe rellenar todo el form.
+
+2. En el segundo html se seleccionan los asientos, los cuales se colorearan de verde si se seleccionan y se descolorearan si se seleccionan de nuevo. 
+    Al terminar de seleccionar todos los asientos, los asientos seleccionados se colorearan de rojo mientras la pagina (con una promesa) "carga".
+
+3. En el tercer html se display la informacion del ticket del usuario con los asientos seleccionados separados por fila y columna individualmente
+
+4. Si se vuelve a comprar se puede comprobar que los asietos ya reservados no se pueden elegir ni seleccionar
+
+Muchas gracias por tomarte el tiempo de corregir todos los desafios y muchas gracias por las clases dictadas!!! 
+Fueron dificil de enteder al principio pero muy faciles de repasar gracias a tu manera de explicar!
+
--- javascript/selectSeats.js
+++ javascript/selectSeats.js
@@ -16,6 +16,22 @@ function blockSeats(array) {
 
 let num1 = 0;
 let takenSeats = [];
+let purchasedStorage = [];
+
+//SEATS TAKEN 
+const seatsStorage = localStorage.getItem("purchased");
+
+//IF LOCAL IS NOT EMPTY THEN
+if (seatsStorage !== null) {
+
+    purchasedStorage = JSON.parse(seatsStorage);
+    purchasedStorage.forEach(e => {
+
+        //CHANGE COLOR TO RED
+        let seat = document.getElementById(e);
+        seat.className = "bi bi-person-fill seats seats-taken"
+    })
+}
 
 // ---- ALL SEATS ---- //
 let seats = document.getElementById("seats");
@@ -57,10 +73,13 @@ thisSeat.addEventListener("click", (e) => {
         } else {
 
             if (takenSeats.length < user.amountSeats) {
+                //IF IS NOT TAKEN THEN
+                if (!purchasedStorage.includes(targetId)) {
 
-                //CHANGE TO GREEN AND ADD
-                seat.className = "bi bi-person-fill seats seats-select"
-                takenSeats.push(targetId);
+                    //CHANGE TO GREEN AND ADD
+                    seat.className = "bi bi-person-fill seats seats-select"
+                    takenSeats.push(targetId);
+                }
 
                 //BUTTON AVIABLE TO PUSH
                 if (takenSeats.length === user.amountSeats) {
@@ -80,17 +99,23 @@ thisSeat.addEventListener("click", (e) => {
 lockSeats.addEventListener("click", () => {
 
     user.seats = takenSeats;
-
-    localStorage.setItem("taken", JSON.stringify(takenSeats));
     localStorage.setItem("reserved", JSON.stringify(user));
 
+    //SAVE INTO LOCAL
+    takenSeats.forEach(e => {
+        purchasedStorage.push(e);
+    });
+    localStorage.setItem("purchased", JSON.stringify(purchasedStorage));
+
+    //CHANGE COLOR TO RED
     blockSeats(takenSeats);
 
+    //MOVING TO NEXT HTML WITH "LOADING"
     lockSeats.innerHTML = "LOADING"
-
     setTimeout(() => {
         window.location.href = "purchase-confirmed.html"
     }, 1000);
+
 })
 
 
